9f93ac465b9a209cc4891ed5b0323a38a5ad579e
MohaamedHamdy <mohaamedhaamdyy@gmail.com>
2024-03-07 09:55:57 +0200

    sign in repo done

---
+++ petowner_frontend/lib/features/registration/signin/data/repo/sign_in_repo.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:petowner_frontend/core/utils/networking/api_service.dart';
+
+class SignInRepo {
+  final ApiService api;
+  final FlutterSecureStorage _storage = const FlutterSecureStorage();
+
+  SignInRepo({required this.api});
+
+  Future<void> login({required String password, required String email}) async {
+    try {
+      var response = await api.post(
+        endPoints: 'PetOwner/SignIn',
+        data: {"password": password, "email": email},
+      );
+      // Check if response is successful
+      if (response.data['status'] == 'Success') {
+        final token = response.data['token'];
+        await _storage.write(key: 'token', value: token);
+      } else {
+        // Handle error response
+        throw Exception('Failed to sign in');
+      }
+    } catch (e) {
+      // Handle other errors
+      debugPrint('Login error: $e');
+      throw e;
+    }
+  }
+}
