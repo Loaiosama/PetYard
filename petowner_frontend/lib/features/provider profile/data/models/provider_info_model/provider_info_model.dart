63edea253d9fe104957273108c9c5fc96d62e708
MohaamedHamdy <mohaamedhaamdyy@gmail.com>
2024-06-24 13:58:35 +0300

    update provider info model to match updates in json response
    added age field and services class which may contain all services provided by one provider.

--- petowner_frontend/lib/features/provider profile/data/models/provider_info_model/provider_info_model.dart
+++ petowner_frontend/lib/features/provider profile/data/models/provider_info_model/provider_info_model.dart
@@ -4,22 +4,32 @@ import 'data.dart';
 
 class ProviderInfoModel extends Equatable {
   final String? status;
-  final Data? data;
+  final Data? data; // Changed from 'data' to 'provider'
+  final List<Service>? services; // Added services field
 
-  const ProviderInfoModel({this.status, this.data});
+  const ProviderInfoModel({this.status, this.data, this.services});
 
   factory ProviderInfoModel.fromJson(Map<String, dynamic> json) {
     return ProviderInfoModel(
       status: json['status'] as String?,
-      data: Data.fromJson(json['data'] as Map<String, dynamic>),
+      data: json['provider'] != null
+          ? Data.fromJson(json['provider'] as Map<String, dynamic>)
+          : null,
+      services: json['services'] != null
+          ? (json['services'] as List)
+              .map((service) =>
+                  Service.fromJson(service as Map<String, dynamic>))
+              .toList()
+          : null,
     );
   }
 
   Map<String, dynamic> toJson() => {
         'status': status,
-        'data': data?.toJson(),
+        'provider': data?.toJson(),
+        'services': services?.map((service) => service.toJson()).toList(),
       };
 
   @override
-  List<Object?> get props => [status, data];
+  List<Object?> get props => [status, data, services];
 }
